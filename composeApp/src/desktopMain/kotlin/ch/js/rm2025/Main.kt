48544b0c716bb6ce899609025e537edf184a63ac
Jan Sigrist <jan.sigrist@ergon.ch>
2025-03-15 11:31:14 +0100

    refactor: enhance window configuration and improve layout in Main.kt

--- composeApp/src/desktopMain/kotlin/ch/js/rm2025/Main.kt
+++ composeApp/src/desktopMain/kotlin/ch/js/rm2025/Main.kt
@@ -1,15 +1,23 @@
 package ch.js.rm2025
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import cafe.adriel.voyager.navigator.Navigator
 import ch.js.rm2025.data.DatabaseFactory
 import ch.js.rm2025.ui.MainWindowScreen
-import cafe.adriel.voyager.navigator.Navigator
 
 fun main() {
     DatabaseFactory.init()
     application {
-        Window(onCloseRequest = ::exitApplication, title = "AthliTrack") {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "AthliTrack",
+            resizable = true,
+            state = WindowState(size = DpSize(1000.dp, 600.dp))
+        ) {
             Navigator(MainWindowScreen())
         }
     }
--- composeApp/src/desktopMain/kotlin/ch/js/rm2025/ui/ProgressScreen.kt
+++ composeApp/src/desktopMain/kotlin/ch/js/rm2025/ui/ProgressScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
